f43c48c21258a8bd5f92aa547f45acf2463f71bd
bellacaicai <152201596+bellacaicai@users.noreply.github.com>
2025-12-09 16:20:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 export {default} from "./e587dfc79d028a3f@134.js";
+import {Runtime, Inspector} from "@observablehq/runtime";
+
+const main = new Runtime().module(define, name => {
+  if (name === "dashboard") {
+    // 把 notebook 中名为 "dashboard" 的 cell 渲染到 #dashboard 这个容器
+    return new Inspector(document.querySelector("#dashboard"));
+  }
+  // 其他所有 cell 都不渲染（返回 null）
+  return null;
+});
